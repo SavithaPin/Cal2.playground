306164525f89c6963ed712972084a0131c4934c0
SavithaPin <prc-lh138-04@PRC-LH138-04s-iMac.local>
2016-07-11 16:07:04 -0500

    Cal2

---
+++ Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Calculator"
+
+let num1 = 3
+let num2 = 5
+
+let add = num1 + num2
+let sub = num1 - num2
+let mul = num1 * num2
+let div = num1 / num2
+let mod = num1 % num2
+let Percentage = 100/30
+
+print("Add = \(add)",terminator: " ")
+print("Sub = \(sub)",terminator: " ")
+print("Mul = \(mul)", terminator: " ")
+print("Div = \(div)", terminator: " ")
+print("Mod = \(mod)", terminator: " ")
+print("Percentage = \(Percentage)", terminator: " ")
